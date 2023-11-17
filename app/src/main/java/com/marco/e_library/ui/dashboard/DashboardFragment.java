d7494129a678a10d6212a73f26de85cfd36f6af4
Marc723 <marcotp723@gmail.com>
2023-11-17 13:31:34 +0800

    Penambahan fitur list

--- app/src/main/java/com/marco/e_library/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/marco/e_library/ui/dashboard/DashboardFragment.java
@@ -1,37 +1,75 @@
 package com.marco.e_library.ui.dashboard;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.marco.e_library.Buku;
+import com.marco.e_library.ListBukuAdapter;
+import com.marco.e_library.R;
 import com.marco.e_library.databinding.FragmentDashboardBinding;
+import com.marco.e_library.databinding.FragmentHomeBinding;
+import com.marco.e_library.ui.home.HomeViewModel;
+
+import java.util.ArrayList;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+    private RecyclerView rvHeroes;
+    private ArrayList<Buku> list = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentDashboardBinding.inflate(inflater, container, false); // Change layout inflater
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        rvHeroes = binding.rvHeroes;
+
+        rvHeroes.setHasFixedSize(true);
+
+        list.addAll(getListHeroes());
+        showRecyclerList();
         return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public ArrayList<Buku> getListHeroes(){
+        String[] dataName = getResources().getStringArray(R.array.data_name);
+        String[] dataDescription = getResources().getStringArray
+                (R.array.data_description);
+        TypedArray dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
+        ArrayList<Buku> listHero = new ArrayList<>();
+        for (int i = 0; i < dataName.length; i++){
+            Buku hero = new Buku();
+            hero.setName(dataName[i]);
+            hero.setDescription(dataDescription[i]);
+            hero.setPhoto(dataPhoto.getResourceId(i, -1));
+            listHero.add(hero);
+        }
+        return listHero;
+    }
+
+
+    private void showRecyclerList() {
+        rvHeroes.setLayoutManager(new LinearLayoutManager(requireContext()));
+        ListBukuAdapter listHeroAdapter = new ListBukuAdapter(list);
+        rvHeroes.setAdapter(listHeroAdapter);
+        listHeroAdapter.setOnItemClickCallback(this::showSelectedHero);
+    }
+
+    private void showSelectedHero(Buku hero) {
+        Toast.makeText(requireContext(), "Kamu Memilih " + hero.getName(), Toast.LENGTH_SHORT).show();
     }
 }

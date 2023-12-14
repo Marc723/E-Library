1838a25ec57c0faac7dc8d20f7443c814771c4b2
Marc723 <marcotp723@gmail.com>
2023-12-14 19:14:42 +0800

    penambahan animasi

--- app/src/main/java/com/marco/e_library/LoginActivity.java
+++ app/src/main/java/com/marco/e_library/LoginActivity.java
@@ -3,6 +3,7 @@ package com.marco.e_library;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -13,13 +14,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import android.util.DisplayMetrics;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText editTextUsername, editTextPassword;
+    private EditText editTextEmail, editTextPassword;
     private Button buttonLogin, buttonRegister;
 
     private FirebaseAuth mAuth;
+    ImageView bgapp, clover;
+    TextView textsplash;
+    ConstraintLayout texthome,icons;
+    Animation frombottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +38,7 @@ public class LoginActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
         buttonLogin = findViewById(R.id.buttonLogin);
         buttonRegister = findViewById(R.id.buttonRegister);
@@ -46,10 +56,25 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
+
+        frombottom = AnimationUtils.loadAnimation(this, R.anim.frombottom);
+        bgapp = (ImageView) findViewById(R.id.bgapp);
+        clover = (ImageView) findViewById(R.id.clover);
+        textsplash = (TextView) findViewById(R.id.textsplash);
+        texthome = (ConstraintLayout) findViewById(R.id.texthome);
+        icons = (ConstraintLayout) findViewById(R.id.icons);
+
+        bgapp.animate().translationY(-1300).setDuration(800).setStartDelay(300);
+        clover.animate().alpha(0).setDuration(800).setStartDelay(600);
+        textsplash.animate().translationY(140).alpha(0).setDuration(800).setStartDelay(300);
+
+        texthome.startAnimation(frombottom);
+        icons.startAnimation(frombottom);
+
     }
 
     private void loginUser() {
-        String email = editTextUsername.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
 
         if (email.isEmpty() || password.isEmpty()) {
